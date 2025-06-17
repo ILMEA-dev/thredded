db4f446bc0e957c972e6630575e6b3ab62dd7e2a
Kevin Voshell <kevinvoshell@gmail.com>
2025-06-16 23:20:24 -0500

    migration

--- db/migrate/20250617040935_add_parent_to_thredded_posts.rb
+++ db/migrate/20250617040935_add_parent_to_thredded_posts.rb
@@ -1,5 +1,5 @@
 class AddParentToThreddedPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :thredded_posts, :parent, null: false, foreign_key: true
+    add_reference :thredded_posts, :parent, foreign_key: { to_table: :thredded_posts }, null: true
   end
 end
